9b8acf5453083ac02656fa8c0014638e1cc4e2ac
Lior Tony Landa <liorlanda@campus.technion.ac.il>
2019-06-20 18:06:44 +0300

    converts initial list according to df to initial list of passages

--- convert_INEXqrels_to_TRECqrels.py
+++ convert_INEXqrels_to_TRECqrels.py
@@ -17,7 +17,8 @@ if __name__ == '__main__':
             with open(temp_file, "w") as temp:
                 temp_path = dp.ensure_file(temp)
                 temp.write(f'{qid} Q0 {name_parts[0]} 1 1 yeet {name_parts[1]} {name_parts[2]}')
-                rel_ret_size = sp.run(f'java -jar inex_eval.jar -f -q {inex_qrels} {temp_path} |grep rel_ret_size', capture_output=True, text=True).stdout
+                rel_ret_size = sp.run(f'java -jar inex_eval.jar -f -q {inex_qrels} {temp_path} |grep rel_ret_size',
+                                      capture_output=True, text=True).stdout
                 rel_ret_size = rel_ret_size.split()[2]
                 if rel_ret_size >= 300:
                     trec_qrels_list.append(f'{qid} 1 {pid} 1')
