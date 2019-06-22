e48d51258cd351f3fd6eef432fb652932e95855a
Zendelo <olegzendel@campus.technion.ac.il>
2019-06-22 09:00:57 -0400

    running version, preparing to convert TF

--- convert_INEXqrels_to_TRECqrels.py
+++ convert_INEXqrels_to_TRECqrels.py
@@ -6,11 +6,11 @@ import pickle
 if __name__ == '__main__':
 
     # initial_res_file = dp.ensure_file("~/ipcs/initial_list")
-    passageDict = pd.read_pickle(
-        "C:\\Users\\liorl\\OneDrive - Altec Business Computing Ltd\\Lior - Do Not Touch!\\Study\\Technion\\Advanced Topics in IR - Technion - 2019\\FInal Project\\ipcsGit\\ipcs\\pkl_files\\full_df.pkl")
-    inex_qrels = "C:\\Users\\liorl\\OneDrive - Altec Business Computing Ltd\\Lior - Do Not Touch!\\Study\\Technion\\Advanced Topics in IR - Technion - 2019\\FInal Project\\ipcsGit\\ipcs\\INEX_Qrels.txt"
-    temp_file = 'C:\\Users\\liorl\\OneDrive - Altec Business Computing Ltd\\Lior - Do Not Touch!\\Study\\Technion\\Advanced Topics in IR - Technion - 2019\\FInal Project\\ipcsGit\\ipcs\\INEX_eval.tmp'
-    trec_qrels = open("C:\\Users\\liorl\\OneDrive - Altec Business Computing Ltd\\Lior - Do Not Touch!\\Study\\Technion\\Advanced Topics in IR - Technion - 2019\\FInal Project\\ipcsGit\\ipcs\\trec_bert_qrels.txt", 'w+')
+    passageDict = dp.ensure_file("~/ipcs/ipcs/pkl_files/full_df.pkl")
+    passageDict = pd.read_pickle(passageDict)
+    inex_qrels = dp.ensure_file("~/ipcs/INEX_Qrels.txt")
+    temp_file = "INEX_eval.tmp"
+    trec_qrels = open("trec_bert_qrels.txt", 'w+')
     trec_qrels_list = []
     for row in passageDict.drop(['rank', 'method'], axis=1).itertuples():
 
@@ -18,16 +18,14 @@ if __name__ == '__main__':
         with open(temp_file, "w") as temp:
             temp_path = temp_file
             temp.write(f'{qid} Q0 {docno} 1 1 yeet {start_idx} {length}')
-            rel_ret_size = sp.run(f'java -jar inex_eval.jar -f -q {inex_qrels} {temp_path} |grep rel_ret_size',
-                                  capture_output=True, text=True).stdout
-            print(rel_ret_size)
-            rel_ret_size = rel_ret_size.split()[2]
-            exit()
-            if int(rel_ret_size) >= 300:
-                trec_qrels.write(f'{qid} 1 {docno}_{start_idx}_{length} 1')
+        inex_eval = dp.ensure_file('~/ipcs/inex_eval.jar')
+        rel_ret_size = sp.run(['/usr/bin/java', '-jar', inex_eval, '-f', '-q', inex_qrels, temp_path], capture_output=True).stdout
+        rel_ret_size = sp.run(['grep', 'rel_ret_size'], input=rel_ret_size, capture_output=True).stdout
+        rel_ret_size = sp.run(['grep', '-v', 'all'], input=rel_ret_size, capture_output=True).stdout
+        rel_ret_size = rel_ret_size.split()[2]
+        if int(rel_ret_size) >= 300:
+            trec_qrels.write(f'{qid} 1 {docno}_{start_idx}_{length} 1\n')
 
-            else:
-                trec_qrels.write(f'{qid} 1 {docno}_{start_idx}_{length} 0')
+        else:
+            trec_qrels.write(f'{qid} 1 {docno}_{start_idx}_{length} 0\n')
 
-    with open('trec_qrels_list.pkl', 'wb') as f:
-        pickle.dump(trec_qrels_list, f)
--- convert_TRECrun_to_TRECrun_with_pid.py
+++ convert_TRECrun_to_TRECrun_with_pid.py
@@ -3,8 +3,8 @@ import PassageExtractor
 import pandas as pd
 
 if __name__ == '__main__':
-    initial_res_file = open("initial_list", 'r')
-    BERT_res_file = open("C:\\Users\\liorl\\OneDrive - Altec Business Computing Ltd\\Lior - Do Not Touch!\\Study\\Technion\\Advanced Topics in IR - Technion - 2019\\FInal Project\\ipcsGit\\ipcs\\ipcsnew_res_list.txt", 'w')
+    initial_res_file = open(dp.ensure_file('~/ipcs/ipcs/initial_list'))
+    BERT_res_file = open("ipcsnew_res_list.txt", 'w')
     # passageDict = {'qid': ["2009001", "2009001", "2009001"],
     #                'docno': ["3260094", "3260094", "32600"],
     #                'docid': ["3260094", "3260094", "32600"],
@@ -13,16 +13,13 @@ if __name__ == '__main__':
     #                'start_index': ["0", "301", "0"],
     #                'len': ["300", "300", "300"],
     #                'pid': ["3260094_0_300", "3260094_301_300", "32600_0_300"]}
-    df = pd.read_pickle("C:\\Users\\liorl\\OneDrive - Altec Business Computing Ltd\\Lior - Do Not Touch!\\Study\\Technion\\Advanced Topics in IR - Technion - 2019\\FInal Project\\ipcsGit\\ipcs\\pkl_files\\full_df.pkl")
+    df = pd.read_pickle(dp.ensure_file('~/ipcs/ipcs/pkl_files/full_df.pkl'))
 
 
-    dealt_docs = set()
     for line in initial_res_file:
         qid, _, docno, rank, score, _ = line.split()
-        if docno not in dealt_docs:
-            dealt_docs.add(docno)
-            curr_df = df.loc[(df['qid'] == int(qid)) & (df['docno'] == docno)]
+        curr_df = df.loc[(df['qid'] == int(qid)) & (df['docno'] == docno)]
 
-            for i, (_, row) in enumerate(curr_df.iterrows()):
-                rank = row['rank']
-                BERT_res_file.write(f'{qid} Q0 {row["docno"]}_{row["start_idx"]}_{row["length"]} {rank} {score} convert\n')
+        for i, (_, row) in enumerate(curr_df.iterrows()):
+            rank = row['rank']
+            BERT_res_file.write(f'{qid} Q0 {row["docno"]}_{row["start_idx"]}_{row["length"]} {rank} {score} convert\n')
